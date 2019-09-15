798c75c4e0fd3c20068f4468fb5dfc2df2307052
Li Song <iseansay@gmail.com>
2019-09-15 15:02:48 +0100

    add user agent to request

--- server/index.js
+++ server/index.js
@@ -76,8 +76,14 @@ const rottenView = (req, res) => {
     }
 
     return getRottenIdByimdbId(imdbId).then(rottenId => {
-        const url = `https://www.rottentomatoes.com/${rottenId}`;
-        return request(url)
+        const uri = `https://www.rottentomatoes.com/${rottenId}`;
+        const options = {
+            uri,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36",
+            }
+        }
+        return request(options)
     }).then(body => {
         const dom = new JSDOM(body)
         const document = dom.window.document
